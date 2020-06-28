b7953b33fd3088ce9dbffada21f08ce3c46c3e60
Tellvinch <tellvinchimani@gmail.com>
2020-06-28 20:41:13 +0300

    add changes

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.todays_picture,name='Todayspicture'),
+    url('^$',views.todays_picture,name='Todayspicture'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_pictures,name = 'pastPictures'),
 ]
