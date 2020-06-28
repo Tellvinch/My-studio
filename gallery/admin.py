2dad0684d0854aecb779e89bb6eb4adc8c308814
Tellvinch <tellvinchimani@gmail.com>
2020-06-28 20:35:19 +0300

    add changes

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Image,Category,Location
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
