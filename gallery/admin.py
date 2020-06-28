bfb2d9872b87d0c0ff9e30c8adb2fbfcc59bd80b
Tellvinch <tellvinchimani@gmail.com>
2020-06-28 20:47:33 +0300

    add changes

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
+from .models import Location,Image,Category,
 
-# Register your models here.
-from django.contrib import admin
-from .models import Image,Category,Location
 
+admin.site.register(Location)
 admin.site.register(Image)
 admin.site.register(Category)
-admin.site.register(Location)
--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.todays_picture,name='Todayspicture'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_pictures,name = 'pastPictures'),
+    url('^$',views.index,name = 'index'),
+    url(r'^image/(\d+)', views.image, name ='image'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^location/(?P<location>.*)', views.get_location, name='location'),
+    url(r'^category/(?P<category>.*)', views.get_category, name='category')
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
