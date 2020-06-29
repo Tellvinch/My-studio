e0acddc16424266f77d38360440a27762794ede4
Tellvinch <tellvinchimani@gmail.com>
2020-06-29 10:45:34 +0300

    add changes

--- photo/admin.py
+++ photo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Images, Category, Location
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "pub_date", "location", "category"]
+    class Meta:
+        model = Images
+
+admin.site.register(Images, ImageAdmin )
+admin.site.register(Category)
+admin.site.register(Location)
--- photo/tests.py
+++ photo/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from .models import Images, Location,Category
 
-# Create your tests here.
+class LocationTest(TestCase):
+    def setUp(self):
+        self.location = Location(name = 'Nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save(self):
+        self.location.save_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) > 0)
+
+class CategoryTest(TestCase):
+    def setUp(self):
+        self.category = Category(name = 'Food')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save(self):
+        self.category.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+class ImageTest(TestCase):
+
+    def setUp(self):
+        self.location = Location(name= 'Nairobi')
+        self.location.save()
+        self.category = Category(name = 'name')
+        self.category.save()
+        self.image = Images(image_path = 'images/1.jpeg', name = 'Tellvinch', description = 'dance accepts everyone', location = self.location, category = self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Images))
+
+    def test_save(self):
+        self.image.save_images()
+        image = Images.objects.all()
+        self.assertTrue(len(image) > 0)
