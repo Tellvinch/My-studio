edfc4d2f091ff2cde979c3369dcd762147f8ee66
Tellvinch <tellvinchimani@gmail.com>
2020-06-30 08:44:49 +0300

    add changes

--- gallery/settings.py
+++ gallery/settings.py
@@ -76,13 +76,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'gallery.urls'
@@ -114,7 +114,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'studio3',
         'USER': 'tellvinch',
-    'PASSWORD':'IMATELLVINCHNI',
+    'PASSWORD':'database',
     }
 }
 
